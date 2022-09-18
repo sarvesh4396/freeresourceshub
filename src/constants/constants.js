06f799a2c54e2b651a5ddee0e8deaf0a715bce35
sarvesh4396 <heysarvesh@pm.me>
2022-09-18 21:01:44 +0530

    add some vars

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,10 @@
+var data = require("./../assets/data.json");
+
 export const SITE_VARS = {
   name: "FreeResources Hub",
   email: "heysarvesh@pm.me",
   github: "https://github.com/sarvesh4396/freeresourceshub",
+  github_issue: "https://github.com/sarvesh4396/freeresourceshub/issues",
+  twitter: "https://twitter.com/sarvesh4396",
+  data: data,
 };
