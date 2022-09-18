c15aad1a82f3027e0658a69d172b978df77cc679
sarvesh4396 <heysarvesh@pm.me>
2022-09-18 16:06:14 +0530

    add background image to full screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="h-screen bg-back-pattern">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
