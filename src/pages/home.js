fb86767bd3ca30122c9e55a7d4104715bd90def3
sarvesh4396 <heysarvesh@pm.me>
2022-09-18 21:34:21 +0530

    improved card
    made card clickable,added icons for site urls

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,13 +3,22 @@ import { SITE_VARS } from "./../constants/constants";
 
 function ProductCard(item) {
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg m-2 border-4 border-slate-200 border-b-slate-500 ">
-      <img class="w-full" src="/img/card-top.jpg" alt={item.name} />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{item.name}</div>
-        <p class="text-gray-700 text-base">{item.description}</p>
+    <a href={item.url} target="_blank" rel="noopener noreferrer">
+      <div class="flex overflow-hidden shadow-lg border-4 border-slate-200 border-b-slate-500 rounded-lg transform transition duration-500 hover:scale-110 ">
+        <img
+          class="object-none w-20 h-20 object-center"
+          src={
+            "https://s2.googleusercontent.com/s2/favicons?sz=64&domain=" +
+            item.url
+          }
+          alt={item.name}
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">{item.name}</div>
+          <p class="text-gray-700 text-base">{item.description}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
 
