73751e5b9e93e4895c3624501d80da6507128cad
sarvesh4396 <heysarvesh@pm.me>
2022-09-18 16:05:41 +0530

    add background pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "back-pattern": "url('./assets/images/background.svg')",
+      },
+    },
   },
   plugins: [],
 };
