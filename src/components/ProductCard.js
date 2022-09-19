e480de7044715f9afbcb5b096b570c65b73a8f35
sarvesh4396 <heysarvesh@pm.me>
2022-09-19 13:00:32 +0530

    changed border color

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export function ProductCard(item) {
   return (
     <a key={item.url} href={item.url} target="_blank" rel="noopener noreferrer">
-      <div className="flex overflow-hidden shadow-lg border-4 border-slate-200 border-b-slate-500 rounded-lg transform transition duration-500 hover:scale-110 ">
+      <div className="flex overflow-hidden shadow-lg border-4 border-blue-300 border-b-slate-500 rounded-lg transform transition duration-500 hover:scale-110 ">
         <img
           className="object-none w-20 h-20 object-center"
           src={
