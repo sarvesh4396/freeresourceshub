06ce72bf82b52467aa16d214f7097df0cc93660a
sarvesh4396 <heysarvesh@pm.me>
2022-09-20 11:23:56 +0530

    added logo image

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,16 +3,18 @@ import { SITE_VARS } from "./../constants/constants";
 
 function Logo() {
   return (
-    <a href="/" className="mr-4 cursor-pointer py-1.5 font-bold">
-      <div className="flex align-middle text-black">
-        {/* <img
-          src={require("./../assets/images/logo.png")}
-          alt="Site Logo"
-          className="h-20 w-20 m-2"
-        /> */}
-        <span className="align-middle">{SITE_VARS.name}</span>
-      </div>
-    </a>
+    <div className="flex items-center flex-shrink-0 text-blue-900 mr-6">
+      <img
+        src={require("./../assets/images/logo.png")}
+        alt="Site Logo"
+        className="h-20 w-20 m-2"
+      />
+      <a href="/" className="mr-4 cursor-pointer py-1.5 font-bold">
+        <div className="font-semibold text-xl tracking-tight">
+          <span className="align-middle">{SITE_VARS.name}</span>
+        </div>
+      </a>
+    </div>
   );
 }
 
